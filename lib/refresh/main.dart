8df1cdbece7c21e2648c00a6f6fdcacbe7a002be
aaa <1289181391@qq.com>
2019-04-04 17:34:39 +0800

    优化上拉加载

--- lib/refresh/main.dart
+++ lib/refresh/main.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -18,65 +17,52 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _pageCode = 1;
-
-  List<String> issues;
-
+  var titles;
   bool loading = false;
 
   @override
   build(context) {
-    var length = issues?.length ?? 0;
+    var length = titles?.length ?? 0;
     return Scaffold(body: Container(
       child: ListView.builder(
-        itemBuilder: (context, int index) {
+        itemBuilder: (context, index) {
           if (index == length) {
             _load();
-            return Center(
-              child: Container(
-                margin: const EdgeInsets.only(top: 8.0),
-                width: 32.0,
-                height: 32.0,
-                child: const CircularProgressIndicator(),
-              ),
-            );
+            return Center(child: RefreshProgressIndicator());
           } else if (index > length) {
             return null;
           }
-          var title = issues[index];
           return Container(
             decoration: BoxDecoration(
                 border:
                     Border(bottom: BorderSide(color: Colors.grey.shade300))),
-            child: ListTile(key: ValueKey<String>(title), title: Text(title)),
+            child: ListTile(
+                key: ValueKey<String>(titles[index]),
+                title: Text(titles[index])),
           );
         },
       ),
     ));
   }
 
-  Future<void> _load() async {
+  _load() async {
     if (loading) {
       return null;
     }
     loading = true;
-    try {
-      var resp = await http.get('https://www.cmtzz.cn/api/v1/interviews?page=$_pageCode');
-      var newsList = json.decode(resp.body)['data']['list'];
-      setState(() {
-        _pageCode += 1;
-        if (newsList is List) {
-          if (issues == null) {
-            issues = <String>[];
-          }
-          newsList.forEach((dynamic e) {
-            if (e is Map) {
-              issues.add(e['title'] as String);
-            }
-          });
+    var resp = await http
+        .get('https://www.cmtzz.cn/api/v1/interviews?page=$_pageCode');
+    var newsList = json.decode(resp.body)['data']['list'];
+    setState(() {
+      print(_pageCode);
+      _pageCode++;
+      if (newsList is List) {
+        if (titles == null) {
+          titles = <String>[];
         }
-      });
-    } finally {
+        newsList.forEach((dynamic e) => titles.add(e['title'] as String));
+      }
       loading = false;
-    }
+    });
   }
 }
